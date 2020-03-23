8232c11d4c81be28d2b5ed3eef78e34c1f7349e8
Maryna <maryna.pylypchenko@gmail.com>
2020-03-23 15:25:28 +0200

    commit

---
+++ Javascript/lesson4/task5/index.js
@@ -0,0 +1,31 @@
+const m = 1;
+const n = 70;
+let sum = 0;
+
+for (
+    for (let i = m; i <= n; i++) {
+        if (i % 2)
+
+
+
+
+    }
+
+
+
+
+
+    if
+    else
+
+    if
+    else
+
+    if
+    else
+
+    if
+    else
+
+
+        console.log(sum)
