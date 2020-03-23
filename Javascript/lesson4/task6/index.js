18fab7622366dc25f16f055e63e45266e3814ca3
Maryna <maryna.pylypchenko@gmail.com>
2020-03-23 17:29:08 +0200

    commit

--- Javascript/lesson4/task6/index.js
+++ Javascript/lesson4/task6/index.js
@@ -1,13 +1,13 @@
-let m = 0;
+let result = 0;
 
 for (let m = 0; m <= 1000; m++) {
-    if (m % 2 == 1) {
+    if (m % 2 != 1) {
         console.log('Found');
-        m += m;
+        result += m;
     }
 }
 
-if (m * 5 > 5000) {
+if (result * 5 > 5000) {
     console.log('Bigger');
 } else {
     console.log('Smaller or equal');
