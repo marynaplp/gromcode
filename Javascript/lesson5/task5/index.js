aa9d104442fcf99267485c45615abbba6eb9c7cb
Maryna <maryna.pylypchenko@gmail.com>
2020-03-25 00:18:27 +0200

    commit

--- Javascript/lesson5/task5/index.js
+++ Javascript/lesson5/task5/index.js
@@ -1,8 +1,9 @@
 function getSum(a, b) {
     let sum = 0;
     for (let i = a; i < b; i++); {
-        if (i % 2 == 0) {
+        if (i % 2 === 0) {
             sum += i;
+            console.log(sum);
         }
     }
     return (sum);
