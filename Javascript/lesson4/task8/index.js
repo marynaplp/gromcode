3b6122ce50f6edba189c26b8aa46182309c8b75a
Maryna <maryna.pylypchenko@gmail.com>
2020-03-24 14:04:23 +0200

    commit

--- Javascript/lesson4/task8/index.js
+++ Javascript/lesson4/task8/index.js
@@ -1,16 +1,14 @@
 let sum = 0;
-
 for (let i = 0; i <= 1000; i++) {
     sum += i;
 };
 
 let whole = sum / 1234;
-let divided = sum % 1234;
+let remainder = sum % 1234;
 
-while (whole < divided) {
+while (whole < remainder) {
     console.log('true');
 }
-while (whole > divided) {
+while (whole > remainder) {
     console.log('false');
-    break;
 }
