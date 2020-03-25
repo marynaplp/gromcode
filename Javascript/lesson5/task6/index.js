aef7054e135c0601db534a9c3fa6de01c12c810a
Maryna <maryna.pylypchenko@gmail.com>
2020-03-25 12:58:09 +0200

    commit

--- Javascript/lesson5/task6/index.js
+++ Javascript/lesson5/task6/index.js
@@ -1,7 +1,7 @@
 function getPrimes(n) {
-    for (let i = 2; i <= n; i++) {
+    nextPrimes: for (let i = 2; i <= n; i++) {
         for (let j = 2; j < i; j++) {
-            if (i % j == 0) continue anotherPrime;
+            if (i % j === 0) continue nextPrimes;
         }
         console.log(i);
     }
