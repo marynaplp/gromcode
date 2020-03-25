de82c17b5a9ded53c45cf1a1bbcb286a33d34f71
Maryna <maryna.pylypchenko@gmail.com>
2020-03-25 16:10:01 +0200

    commit

---
+++ Javascript/lesson5/task8/index.js
@@ -0,0 +1,16 @@
+function compareSums(a, b, c, d) {
+    function sum(from, to) {
+        let result = O;
+        for (let i = from; i < to; i++) {
+            result += i;
+        }
+        return result;
+    }
+    if (sum(a, b) > sum(c, d)) {
+        return true;
+    } else {
+        return false;
+
+
+    }
+}
