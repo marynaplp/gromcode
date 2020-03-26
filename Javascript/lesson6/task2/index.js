6325132585d388fd24b33ae89deb430b3b349058
Maryna <maryna.pylypchenko@gmail.com>
2020-03-26 18:02:22 +0200

    commit

---
+++ Javascript/lesson6/task2/index.js
@@ -0,0 +1,14 @@
+//перебор массивов
+const numbers = [1, 2, 3, 4, 5, 6]
+
+function getSum(numbers) {
+    let sumOfNumbers = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sumOfNumbers += numbers[i];
+        if (!Array.isArray(numbers)) {
+            return null;
+        }
+    }
+    return sumOfNumbers;
+
+}
