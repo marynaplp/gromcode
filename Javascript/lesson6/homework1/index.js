703dfa7d9548238fb72ff2e4d2002b990b6ec576
Maryna <maryna.pylypchenko@gmail.com>
2020-03-27 16:37:47 +0200

    commit

---
+++ Javascript/lesson6/homework1/index.js
@@ -0,0 +1,5 @@
+function createEmptyArray(len) {
+    let array = [];
+    return createEmptyArray;
+
+}
---
+++ Javascript/lesson6/homework2/index.js
@@ -0,0 +1,10 @@
+function squareArray(arr) {
+    let array = [];
+    for (let i = 0; i < arr.length; i++) {
+        array.push(arr[i] * arr[i]);
+        if (!Array.isArray(arr)) {
+            return null;
+        }
+    }
+    return array;
+}
