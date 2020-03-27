01b0ca0f915ef906c1e6f2ddf2cacae2af2213ce
Maryna <maryna.pylypchenko@gmail.com>
2020-03-27 22:38:46 +0200

    commit

---
+++ Javascript/lesson6/hw5/index.js
@@ -0,0 +1,11 @@
+function reverseArray(arr) {
+    if (!Array.isArray(arr)) return null;
+
+    let result = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        newArray.push(arr[i]);
+    }
+    return result
+
+
+}
