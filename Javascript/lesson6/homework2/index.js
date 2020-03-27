0b9d66eb9d8c06dcf3a8df4475bc089877fcb35e
Maryna <maryna.pylypchenko@gmail.com>
2020-03-27 17:13:14 +0200

    commit

--- Javascript/lesson6/homework2/index.js
+++ Javascript/lesson6/homework2/index.js
@@ -1,9 +1,8 @@
 function squareArray(arr) {
-    let squaredArr = [];
+    let squareArr = [];
     if (!Array.isArray(arr)) {
         return null;
-    }
-    for (let i = 0; i < arr.length; i++) {
+    } else {
         squaredArr.push(arr[i] * arr[i])
     }
     return squaredArr;
