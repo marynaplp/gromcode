3532158ac4b66f88dfed5d8a331a375e97ea4b10
Maryna <maryna.pylypchenko@gmail.com>
2020-03-27 21:24:57 +0200

    commit

--- Javascript/lesson6/hw3/index.js
+++ Javascript/lesson6/hw3/index.js
@@ -1,11 +1,10 @@
-function checkSum(arr) {
+const checkSum = arr => {
     if (!Array.isArray(arr)) {
         return null;
-    }
-    let res = arr.reduce((accumulator, currentValue) => accumulator + currentValue);
-    if (res > 100) {
-        return true;
+        let res = arr.reduce((accumulator, currentValue) => accumulator + currentValue);
+        if (res > 100) {
+            return true;
 
-    } else { return false; }
-}
+        } else { return false; }
+    }
 }
