4b896a7d3cc3c5343a1ba2f68ade7585d86af1c2
Maryna <maryna.pylypchenko@gmail.com>
2020-03-28 17:28:31 +0200

    commit

---
+++ Javascript/lesson6/hw11/index.js
@@ -0,0 +1,10 @@
+function includes(arr, num) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++);
+    if (arr[i] === num) {
+        return true;
+    } else {
+        return false;
+    }
+    return result;
+}
