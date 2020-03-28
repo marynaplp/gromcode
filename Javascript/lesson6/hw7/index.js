4962f1d57e83ad1d0f4d1c05dc5067cadf936e52
Maryna <maryna.pylypchenko@gmail.com>
2020-03-28 13:40:52 +0200

    commit

--- Javascript/lesson6/hw7/index.js
+++ Javascript/lesson6/hw7/index.js
@@ -2,14 +2,13 @@ function checker(arr) {
     if (!Array.isArray(arr)) {
         return null;
     }
-    let sumArr = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sumArr += arr[i];
-        console.log(sumArr(i));
-    }
-    if (sumArr > 1000) {
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    if ((max + min) > 1000) {
         return true;
+
     } else {
         return false;
     }
+
 }
