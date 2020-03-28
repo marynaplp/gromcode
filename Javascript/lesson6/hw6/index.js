047ef30d51b3927561cf03fe9fe86fed1459353b
Maryna <maryna.pylypchenko@gmail.com>
2020-03-28 12:45:00 +0200

    commit

---
+++ Javascript/lesson6/hw6/index.js
@@ -0,0 +1,10 @@
+function cloneArr(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    let clonedArr = [];
+    for (let i = 0; i < cloned.length; i++) {
+        cloned[i] = Clone(clonned[i]);
+    }
+    return clonned;
+}
