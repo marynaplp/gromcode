e1fbfdd20cc7304462b4481fcd6663fc0438be54
Maryna <maryna.pylypchenko@gmail.com>
2020-03-28 14:55:01 +0200

    commit

--- Javascript/lesson6/hw8/index.js
+++ Javascript/lesson6/hw8/index.js
@@ -1,14 +1,26 @@
-fuction sortAsc(array) {
-    for (a = 0; a < array.length; i++);
-    for (b = 0; b < array.length; i++);
-
-
+function sortAsc(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[i] < array[j]) {
+                let current = array[a];
+                array[i] = array[j];
+                array[j] = current;
+            }
+        }
+    }
     return array;
 }
 
-function sortDesc(array) {
-    for (a = 0; a < array.length - 1; i++);
-    for (b = 0; b < array.length - 1; i++);
-    return array;
 
+function sortDesc(array) {
+    for (let i = 0; a < array.length; i++) {
+        for (let b = 0; b < array.length; j++) {
+            if (array[i] > array[j]) {
+                let current = array[i];
+                array[i] = array[j];
+                array[i] = current;
+            }
+        }
+    }
+    return array;
 }
