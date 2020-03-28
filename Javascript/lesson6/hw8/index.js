49d213b3426f29445385369a7da0eeda759f4c6c
Maryna <maryna.pylypchenko@gmail.com>
2020-03-28 14:39:34 +0200

    commit

---
+++ Javascript/lesson6/hw8/index.js
@@ -0,0 +1,15 @@
+fuction sortAsc(array) {
+    for (a = 0; a < array.length; i++);
+    for (b = 0; b < array.length; i++);
+    sortAsc((a, b) => a - b);
+
+    return array;
+}
+
+function sortDesc(array) {
+    for (a = 0; a < array.length; i++);
+    for (b = 0; b < array.length; i++);
+    sortDesc((a, b) => b - a);
+    return array;
+
+}
