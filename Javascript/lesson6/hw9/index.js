4f31222f169b6261aecf5b23fc73ba4950200a17
Maryna <maryna.pylypchenko@gmail.com>
2020-03-28 15:31:17 +0200

    commit

--- Javascript/lesson6/hw9/index.js
+++ Javascript/lesson6/hw9/index.js
@@ -1,8 +1,8 @@
 function withdraw(clients, balances, client, amount) {
     for (let j = 0; j < clients.length; j++) {
         if (clients[j] == client) {
-            if (balances[i] - amount >= 0) {
-                return balances[i] - amount;
+            if (balances[i] > amount) {
+                return balances[i] -= amount;
             } else {
                 return -1;
             }
