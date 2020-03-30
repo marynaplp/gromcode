c659abd4192abe99a23fad0e96cd042c64e7c2e0
Maryna <maryna.pylypchenko@gmail.com>
2020-03-30 16:12:16 +0300

    commit

---
+++ Javascript/lesson7/HW2/index.js
@@ -0,0 +1,8 @@
+const reverseArray = numbers => {
+        let reversedArray = [];
+        if (Array.isArray(arr)) {
+            for (let i = 0; i < arr.length; i++) {
+                reversedArr.unshift(arr[i]);
+            }
+
+        }
