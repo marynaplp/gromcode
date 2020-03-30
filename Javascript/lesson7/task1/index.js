f6963b1a4903762e78c795fc7a3be9b530b1e9be
Maryna <maryna.pylypchenko@gmail.com>
2020-03-30 13:04:48 +0300

    commit

--- Javascript/lesson7/task1/index.js
+++ Javascript/lesson7/task1/index.js
@@ -1,6 +1,7 @@
 const getSpecialNumbers = numbers => {
     let specialNumbers = [];
-    for (let i = 0; i < numbers; i++) {
+    for (let i = 0; i < numbers.length; i++) {
+
         if (numbers[i] % 3 === 0) {
             specialNumbers.push(numbers[i]);
 
