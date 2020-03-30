bcbd88b717d07b32d5cedc191c1dda15ffdeedbb
Maryna <maryna.pylypchenko@gmail.com>
2020-03-30 16:41:36 +0300

    commit

---
+++ Javascript/lesson7/HW3/index.js
@@ -0,0 +1,15 @@
+function increaseEvenEl(arr, delta) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    return arr.map(el => {
+        if (el % 2 === 0) {
+            el += delta;
+            return el;
+        } else {
+            return el;
+        }
+    });
+
+
+}
