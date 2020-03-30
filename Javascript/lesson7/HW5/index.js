25e5ef111da87e635a24eb854b3dd759e43f669c
Maryna <maryna.pylypchenko@gmail.com>
2020-03-30 18:57:37 +0300

    commit

--- Javascript/lesson7/HW5/index.js
+++ Javascript/lesson7/HW5/index.js
@@ -1,8 +1,8 @@
 function withdraw(clients, balances, client, amount) {
-    let i = client.findIndex(el => el === client);
-    if (clients[i] == client) {
-        if (balances[i] > amount) {
-            return balances[i] -= amount;
+    let resut = client.findIndex(el => el === client);
+    if (clients[clientsPr] == client) {
+        if (balances[clientsPr] > amount) {
+            return balances[clientsPr] -= amount;
         }
         if (balances[i] < amount) {
             return -1;
