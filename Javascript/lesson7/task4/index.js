ecd58c5e9c004655da50a8e4199835a3d0168391
Maryna <maryna.pylypchenko@gmail.com>
2020-03-30 14:48:06 +0300

    commit

--- Javascript/lesson7/task4/index.js
+++ Javascript/lesson7/task4/index.js
@@ -1,12 +1,9 @@
-const getMessagesForBestStudents = (allSudents, failedStudents) => {
-    const bestStudents = allStudents
-        .filter(name => !(failedStudents.IndexOf(name) !== -1))
+const getMessagesForBestStudents = (allSudents, failedStudents) =>
+    allStudents
+    .filter(name => !(failedStudents.includes(name))
         .map(name => 'Good job, ' + name);
-    return bestStudents;
 
-
-};
-const allStudents = ['Ann', 'Tom', 'Bob', 'Kate'];
-const failedStudents = ['Tom', 'Bob'];
-// Find passed students
-//add message Good job
+        const allStudents = ['Ann', 'Tom', 'Bob', 'Kate'];
+        const failedStudents = ['Tom', 'Bob'];
+        // Find passed students
+        //add message Good job
