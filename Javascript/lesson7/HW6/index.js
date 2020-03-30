6904979be193f5275630c743586fedd3e071b61f
Maryna <maryna.pylypchenko@gmail.com>
2020-03-30 19:22:54 +0300

    commit

---
+++ Javascript/lesson7/HW6/index.js
@@ -0,0 +1,11 @@
+function sum(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    let result = arr.reduce(fuction(sum, num) {
+        return sum + num
+
+    });
+    return result;
+
+}
