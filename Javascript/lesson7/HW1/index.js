18046786639fac603ffb672f19cf9d04c3943abc
Maryna <maryna.pylypchenko@gmail.com>
2020-03-30 15:57:00 +0300

    coomit

---
+++ Javascript/lesson7/HW1/index.js
@@ -0,0 +1,9 @@
+const squareArray = arr => {
+    let newArr = [];
+    if (Array.isArray(arr)) {
+        newArr = arr.map(num => num * num)
+    } else {
+        return null;
+    }
+    return newArr;
+}
