5de4f0f3c663919e121e806fda8c009ef7c46e0e
Maryna <maryna.pylypchenko@gmail.com>
2020-03-31 23:18:36 +0300

    commit

--- Javascript/lesson8/HW8/index.js
+++ Javascript/lesson8/HW8/index.js
@@ -1,7 +1,11 @@
 const buildObject = (keysList, valuesList) => {
-    let newArr = [];
-    for (let i = 0; i < keysList.length; i++);
-    newArr.push([keysList[i], valuesList[i]]);
-    let newObj = Object.fromEntries(newArr);
-    return newObj;
-}
+    const arr = [];
+
+    for (let i = 0; i < keysList.length; i++) {
+        arr.push([keysList[i], valuesList[i]]);
+    }
+
+    const obj = Object.fromEntries(arr);
+
+    return obj;
+};
