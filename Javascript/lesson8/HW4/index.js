2ab41fb0b209a48f4650477f15fb0181b37c0e8f
Maryna <maryna.pylypchenko@gmail.com>
2020-03-31 15:44:28 +0300

    commit

---
+++ Javascript/lesson8/HW4/index.js
@@ -0,0 +1,31 @@
+const userData = {
+    name: "Sam",
+}
+const userId = '12890'
+
+
+
+function addPropertyV1(userData, userId) {
+    userData.Id = userId;
+    return userData;
+}
+
+function addPropertyV2(userData, userId) {
+    userData.Id = userId; // значение
+    const NewObj = Object.assign({}, userData);
+    return NewObj;
+}
+
+function addPropertyV3(userData, userId) {
+    userData.Id = userId;
+    const NewObj = Object.assign({}, userData);
+    NewObj.id = userId;
+    return NewObj;
+}
+
+function addPropertyV3(userData, userId) {
+    userData.Id = userId;
+    const NewObj = {...userData };
+    NewObj.id = userId;
+    return NewObj;
+}
