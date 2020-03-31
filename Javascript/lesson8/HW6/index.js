805170beb68e93ba554b9a00d1a0e1fa3e03906d
Maryna <maryna.pylypchenko@gmail.com>
2020-03-31 17:48:27 +0300

    commit

--- Javascript/lesson8/HW6/index.js
+++ Javascript/lesson8/HW6/index.js
@@ -1,20 +1,20 @@
-const MergeObjectsV1 = (obj1, obj2) => {
-    const newObj = Object.assign({}, obj1, obj2)
-    return newObj
+const mergeObjectsV1 = (obj1, obj2) => {
+    const newObj = Object.assign({}, obj1, obj2);
+    return newObj;
 
 }
-const MergeObjectsV2 = (obj1, obj2) => {
-    const mewObj = Object.assign({}, obj2, obj1)
-    return newObj
+const mergeObjectsV2 = (obj1, obj2) => {
+    const newObj = Object.assign({}, obj2, obj1);
+    return newObj;
 }
-const MergeObjectsV3 = (obj1, obj2) => {
-    const newObj = {...obj1, ...obj2 }
-    return newObj
+const mergeObjectsV3 = (obj1, obj2) => {
+    const newObj = {...obj1, ...obj2 };
+    return newObj;
 
 }
-const MergeObjectsV4 = (obj1, obj2) => {
-    const newObj = {...obj2, ...obj1 }
+const mergeObjectsV4 = (obj1, obj2) => {
+    const newObj = {...obj2, ...obj1 };
 
-    return newObj
+    return newObj;
 
 }
