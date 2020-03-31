a33b85076f0362b24748938792da1c4c521b0aee
Maryna <maryna.pylypchenko@gmail.com>
2020-03-31 15:47:14 +0300

    commit

---
+++ Javascript/lesson8/HW5/index.js
@@ -0,0 +1,15 @@
+function addPropertyV1(obj, key, value) {
+
+}
+
+function addPropertyV2(obj, key, value) {
+
+}
+
+function addPropertyV3(obj, key, value) {
+
+}
+
+function addPropertyV4(obj, key, value) {
+
+}
