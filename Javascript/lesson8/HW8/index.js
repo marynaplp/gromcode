19005f9c94d4ca9f5d79423655a98ad2d0109cb1
Maryna <maryna.pylypchenko@gmail.com>
2020-03-31 22:42:32 +0300

    commit

--- Javascript/lesson8/HW8/index.js
+++ Javascript/lesson8/HW8/index.js
@@ -2,5 +2,7 @@ const buildObject = (keyList, valueList) => {
     let result = [];
     for (let i = 0; i < keyList.length; i++);
     result.push([keyList[i], valueList[i]]);
+
+
     return Object;
 }
