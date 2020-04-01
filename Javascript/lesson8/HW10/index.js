2605e2c1960df6b043e6e9c950d032f3dab5dc96
Maryna <maryna.pylypchenko@gmail.com>
2020-04-01 12:20:14 +0300

    commit

--- Javascript/lesson8/HW10/index.js
+++ Javascript/lesson8/HW10/index.js
@@ -1,10 +1,10 @@
-const Obj = { 'John Doe': 19, 'Tom': 17, 'Bob': 18 };
+//const Obj = { 'John Doe': 19, 'Tom': 17, 'Bob': 18 };
 
 const getAdults = (object) => {
     const getAdults = {};
     for (let age in object) {
         if (age => 18) {
-            adults[age] = obj[age]
+            getAdults[age] = obj[age]
         }
     }
     return getAdults;
