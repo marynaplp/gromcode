9665b711b2bfffe56f899b01299a127a2ca4a511
Maryna <maryna.pylypchenko@gmail.com>
2020-04-01 22:54:34 +0300

    commit

--- Javascript/lesson9/task1/index.js
+++ Javascript/lesson9/task1/index.js
@@ -2,18 +2,18 @@
      .filter(user => user[1] >= 18)
      .map(user => user[0]); //прошлись по  массиву и его вернули
 
- const getAdults = userObj => {
-     // {'Tom': 17, 'John Doe':19,} =>[ ['Tom:' 17],['John Doe', 19] ]
-     const userArray = Object.entries(usersObj);
-     //  c помощьюObject.Entries преобразил в массив массивов;(в большом массиве находится маленький массив)
+ //const getAdults = userObj => {
+ // {'Tom': 17, 'John Doe':19,} =>[ ['Tom:' 17],['John Doe', 19] ]
+ //  const userArray = Object.entries(usersObj);
+ //  c помощьюObject.Entries преобразил в массив массивов;(в большом массиве находится маленький массив)
 
-     // {'Tom': 17, 'John Doe':19,} =>[ ['John Doe', 19] ] 
-     const filteredUsersArray = userArray // отфлитовала массив
-         .filter(user => user[1] >= 18); // получился массив массивов но  с меньшим к-ством элементов
+ // {'Tom': 17, 'John Doe':19,} =>[ ['John Doe', 19] ] 
+ //const filteredUsersArray = userArray // отфлитовала массив
+ //  .filter(user => user[1] >= 18); // получился массив массивов но  с меньшим к-ством элементов
 
-     //Достали ['John Doe', 19] ] 
-     const userNames = filteredUsersArray // прошлись по  массив массивом и взяли один елемент 
-         .map(user => user[0]);
-     return usersNames; // вернули его 
- }
- console.log(getAdults(users));
+ //Достали ['John Doe', 19] ] 
+ //const userNames = filteredUsersArray // прошлись по  массив массивом и взяли один елемент 
+ //   .map(user => user[0]);
+ //return usersNames; // вернули его 
+ //}
+ //console.log(getAdults(users));
