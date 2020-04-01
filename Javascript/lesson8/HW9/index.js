0d246cb11e69197ea5cc858196f13756a59dc197
Maryna <maryna.pylypchenko@gmail.com>
2020-04-01 11:53:33 +0300

    commit

--- Javascript/lesson8/HW9/index.js
+++ Javascript/lesson8/HW9/index.js
@@ -1,8 +1,15 @@
+const mainArr = ['a', 17.1, 'John Doe'];
+const mainObj = { a: 'a', '17.1': 17.1, 'John Doe': 'John Doe' };
+
+
+
 const pickProps = (obj, arr) => {
-    let newObj = {};
+    const newObj = {};
     for (let i = 0; i < arr.length; i++) {
         const key = arr[i];
         newObj[key] = obj[key];
     }
     return newObj;
 }
+const newObj = pickProps(mainArr, mainObj);
+console.log(newObj)
