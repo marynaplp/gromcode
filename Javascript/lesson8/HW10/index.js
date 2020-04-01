d01744422aeb93dbf0a7b941221cd0cc8cf38113
Maryna <maryna.pylypchenko@gmail.com>
2020-04-01 12:25:34 +0300

    commit

--- Javascript/lesson8/HW10/index.js
+++ Javascript/lesson8/HW10/index.js
@@ -3,7 +3,7 @@ const getAdults = { 'John Doe': 19, 'Tom': 17, 'Bob': 18 };
 const getAdults = (object) => {
     const getAdults = {};
     for (let age in object) {
-        if (age => 18) {
+        if (object[age] = 18) {
             getAdults[age] = object[age]
         }
     }
