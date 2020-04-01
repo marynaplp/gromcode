d2d7d416e6c6c0480dc91ff14714cdb5b7672276
Maryna <maryna.pylypchenko@gmail.com>
2020-04-01 20:43:49 +0300

    commit

--- Javascript/lesson9/task1/index.js
+++ Javascript/lesson9/task1/index.js
@@ -2,9 +2,9 @@
      const adultsArray = {};
      const adultsArray = Object.entries(adultsObj)
      const filteredAdultsArray = adultsArray;
-     filter(adult => adult[1] >= 18);
+     filter(adult => adult[1] > 18);
      const adults = filteredAdultsArray
          .map(adult => adult[0]);
-     return adults
+     return adults;
 
  }
