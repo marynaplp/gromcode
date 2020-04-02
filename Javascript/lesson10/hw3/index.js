84740553b72e83b1249fa069900346813c055f11
Maryna <maryna.pylypchenko@gmail.com>
2020-04-02 20:09:20 +0300

    commmit

---
+++ Javascript/lesson10/hw3/index.js
@@ -0,0 +1,9 @@
+function superRound(num, round) {
+    const result = Math.floor(num, round);
+    return [
+        Math.floor(num * result) / result,
+        Math.round(num * result) / result,
+        Math.ceil(num * result) / result,
+        Math.trunc(num * result) / result, +num.toFixed(2),
+    ]
+}
