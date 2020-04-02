900ee74fb733b7347b0a64377e2051e0108e388f
Maryna <maryna.pylypchenko@gmail.com>
2020-04-02 17:34:51 +0300

    commit

--- Javascript/lesson10/hw2/index.js
+++ Javascript/lesson10/hw2/index.js
@@ -1,6 +1,7 @@
 const getRandomNumbers = (length, start, end) => {
-
-
+    if (Math.floor(start) >= Math.floor(end)) {
+        return null;
+    }
     return Array(length).fill().map(el => Math.floor(Math.random() * start + end))
 
 }
