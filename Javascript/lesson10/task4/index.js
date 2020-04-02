cc6ca804c733159cc54b1a6fe0eb1b19f1fce614
Maryna <maryna.pylypchenko@gmail.com>
2020-04-02 15:25:28 +0300

    commit

---
+++ Javascript/lesson10/task4/index.js
@@ -0,0 +1,6 @@
+const getMaxAbsoluteNumber = arr => {
+    if (!Array.isArray(arr) || arr.length == 0) return null
+    const absoluteValues = arr.map(num => Math.abs(num))
+    return Math.max(...absoluteValues);
+
+}
