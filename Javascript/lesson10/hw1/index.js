686a1df405bb4382ea8b761121e728a65336b0c1
Maryna <maryna.pylypchenko@gmail.com>
2020-04-02 16:49:50 +0300

    commit

--- Javascript/lesson10/hw1/index.js
+++ Javascript/lesson10/hw1/index.js
@@ -1,4 +1,4 @@
-const getTotalPrice = num => {
+const getTotalPrice = arr => {
     const sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue)
     return '$' + Math.floor(sum * 100) / 100;
 }
