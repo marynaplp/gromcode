16f9075e0fd1d1658de462b2e825f01871d23c15
Maryna <maryna.pylypchenko@gmail.com>
2020-04-02 14:34:28 +0300

    commit

--- Javascript/lesson10/task2/index.js
+++ Javascript/lesson10/task2/index.js
@@ -1,8 +1,8 @@
-const getParsedIntegers = num => {
-    return num.filter(item => Number.parselnt(item))
+const getParsedIntegers = arr => {
+    return arr.map(item => Number.parselnt(item))
 }
 
-const getParsedIntegersV2 = num => {
+const getParsedIntegersV2 = arr => {
     return num.filter(item => parseInt(item))
 
 }
