9f1be7bcb3aa79ba107a4163a1abf3936e372ca3
Maryna <maryna.pylypchenko@gmail.com>
2020-04-02 15:06:14 +0300

    commit

--- Javascript/lesson10/task3/index.js
+++ Javascript/lesson10/task3/index.js
@@ -2,7 +2,7 @@ const multiRound = num => [
     Math.floor(num * 100) / 100,
     Math.round(num * 100) / 100,
     Math.ceil(num * 100) / 100,
-    Math.trunc(num * 100) / 100,
-    num.toFixed(2)
+    Math.trunc(num * 100) / 100, +num.toFixed(2),
 
 ];
+console.log(muiltiRound(-Math.Pi));
