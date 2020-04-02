eb061e21887d3518ffc50d5c67c5460777ca0804
Maryna <maryna.pylypchenko@gmail.com>
2020-04-02 14:04:06 +0300

    commit

---
+++ Javascript/lesson10/task1/index.js
@@ -0,0 +1,16 @@
+const getFiniteNumbers = num => {
+    return num.filter(item => Number.isFinite(item))
+}
+const getFiniteNumbersV2 = num => {
+    return num.filter(item => isFinite(item))
+}
+const getNaN = num => {
+    return num.filter(item => Number.isNaN(item))
+}
+const getNaNV2 = num => {
+    return num.filter(item => Number.isNaN(item))
+
+}
+const getIntegers = num => {
+    return num.filter(item => Number.isIntegers(item))
+}
