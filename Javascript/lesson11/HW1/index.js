5ea8cb3840052bbd6f2fd24b2a4458a3bc7090f1
Maryna <maryna.pylypchenko@gmail.com>
2020-04-03 16:38:47 +0300

    commit

--- Javascript/lesson11/HW1/index.js
+++ Javascript/lesson11/HW1/index.js
@@ -5,6 +5,7 @@ const splitString = (str, num = 10) => {
     if (str === !String) {
         return null;
     }
+
     while (true) {
         let chunk = str.substr(startPosition, num);
         if (chunk.length === 0) {
@@ -12,10 +13,9 @@ const splitString = (str, num = 10) => {
         }
         if (chunk.length < num) {
             chunk = chunk + ".".repeat(num - chunk.length);
-
         }
         strArr.push(chunk.slice(0));
         startPosition += num;
     }
-    return strtArr;
-}
+    return strArr;
+};
