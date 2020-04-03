33616a6f53567df316bceadfb835c16a1958a1b5
Maryna <maryna.pylypchenko@gmail.com>
2020-04-03 14:00:16 +0300

    commit

---
+++ Javascript/lesson11/task1/index.js
@@ -0,0 +1,27 @@
+const splitText = (text, len) => {
+    const strArr = [];
+    let startPosition = 0;
+    if (text === !String) {
+        return null;
+    }
+    while (true) {
+        let chunk = text.substr(startPosition, len);
+        if (chunk.length === 0) {
+            break;
+        }
+
+    }
+    if (len === underfined) {
+        strArr.push(chunk[0].toUpperCase() + chulk.slice(1));
+        startPosition += 10;
+
+    } else {
+
+        strArr.push(chunk[0].toUpperCase() + chulk.slice(1));
+        startPosition += len;
+
+
+    }
+    return strArr.join('\n');
+
+};
