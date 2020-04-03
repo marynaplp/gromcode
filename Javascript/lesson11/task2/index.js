09d25298fa4a585d80dec1a329286178dae7ffc4
Maryna <maryna.pylypchenko@gmail.com>
2020-04-03 15:13:36 +0300

    commit

--- Javascript/lesson11/task2/index.js
+++ Javascript/lesson11/task2/index.js
@@ -1,8 +1,18 @@
-const sortContacts = contacts => {
+const sortContacts = (contacts, boolean) => {
+    if (!Array.isArray(contacts)) {
+        return null;
+    }
+
     const result = contacts
         .sort((a, b) => {
             return a.localeCompare(b);
         });
+    if (boolean === false) {
+        contacts.sort((a, b) => {
+            return b.localeCompare(a);
+        })
+    }
+
     return result;
 
 }
