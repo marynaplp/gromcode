a74b80efd18ccdbc20a0580a5d0954f36e548b0f
Maryna <maryna.pylypchenko@gmail.com>
2020-04-05 00:03:58 +0300

    commit

---
+++ Javascript/lesson13/task1/index.test.js
@@ -0,0 +1,14 @@
+it('17 равно 17', () => {
+    expect(17).toEqual(17);
+});
+
+it('18 не равно 17', () => {
+    expect(18).not.toEqual(17)
+});
+const getEventNumbers = numbers =>
+    numbers.filter(el => (el % 2 == 0));
+it('should get only even numbers from array', () => {
+    const result = getEvenNumbers([1, 2, 3, 4, 5, 6]);
+    expect(result).toEqual([2, 4, 5]);
+
+});
