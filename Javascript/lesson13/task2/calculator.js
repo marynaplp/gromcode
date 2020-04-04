112a5846751b7512c2cefa69bc8e49a2a0b7120e
Maryna <maryna.pylypchenko@gmail.com>
2020-04-05 00:45:21 +0300

    commit

--- Javascript/lesson13/task2/calculator.js
+++ Javascript/lesson13/task2/calculator.js
@@ -1,7 +1,7 @@
-export const getSquareedNumbers = arr =>
+export const getSquaredNumbers = arr =>
     arr.map(num => num * num);
 
-export const getOddNum = arr =>
+export const getOddNumbers = arr =>
     arr.filter(num => (num % 2 === 1));
 
-export default sum = (a, b) => a + b;
+export default (a, b) => a + b;
