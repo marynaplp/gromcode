71c8b57e76668833648e76d088e369f4138b5ada
Maryna <maryna.pylypchenko@gmail.com>
2020-04-05 00:38:16 +0300

    commit

--- Javascript/lesson13/task2/index.test.js
+++ Javascript/lesson13/task2/index.test.js
@@ -5,3 +5,12 @@ it('should get squaed numbers', () => {
     expect(result).toEqual();
 
 });
+it('should keep odd numbers only', () => {
+    const result = getOddNumbers([1, 2, 3, 4, 5]);
+    expect(result).toEqual([1, 3, 5]);
+});
+
+it('should get sum of numbers', () => {
+    const result = getSum(8, 4);
+    expect(result).toEqual(12);
+});
