ab186741175e9fd002bce5eda7c0bcfe03c337a3
Maryna <maryna.pylypchenko@gmail.com>
2020-04-07 14:30:28 +0300

    commit

--- Javascript/lesson14/task2/index.js
+++ Javascript/lesson14/task2/index.js
@@ -1,23 +1,22 @@
- export function createMessenger() {
-     let message = 'Hi!'
+ export default function createMessenger() {
+     let message = 'Just learn it!';
+     let sender = 'Gromcode';
 
-     export function sendMessage(name) {
-         const sender = 'Gromcode';
-         console.log(`${name}, ${message}.Your ${sender}`)
+     function sendMessage(name) {
+
+         console.log(`${name}, ${message}.Your ${sender}`);
      }
 
-     export function setMessage(text) {
-         message = test;
+     function setMessage(text) {
+         message = text;
+     }
+
+     function setSender(text) {
+         sender = text;
      }
      return {
          sendMessage,
          setMessage,
+         setSender,
      };
  };
- const messenger1 = createMessenger();
- messenger1.sendMessage('Bob');
- const messenger2 = createMessenger();
- messenger2.sendMessage('Tom');
- messenger2.setMessage('Hello');
-
- messenger1.sendMessage('Ann');
