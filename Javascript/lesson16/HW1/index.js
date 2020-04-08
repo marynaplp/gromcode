b5b3e7f96cd28bc21a1fbd40a333f61d8ee83f71
Maryna <maryna.pylypchenko@gmail.com>
2020-04-08 18:04:55 +0300

    commit

--- Javascript/lesson16/HW1/index.js
+++ Javascript/lesson16/HW1/index.js
@@ -2,7 +2,7 @@ function createArrayFunctions(num) {
     let arr = [];
     if (num == undefined) return [];
     if (typeof num !== 'number') return null;
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < num; i++) {
         arr[i] = function() {
             return i;
         }
