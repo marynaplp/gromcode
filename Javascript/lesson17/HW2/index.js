32e47d791a7ff97372d8b9f3287c25130a51fdd3
Maryna <maryna.pylypchenko@gmail.com>
2020-04-13 21:00:33 +0300

    commit

--- Javascript/lesson17/HW2/index.js
+++ Javascript/lesson17/HW2/index.js
@@ -78,5 +78,4 @@ const anotherTransaction = {
 // вызовите метод transaction.printTransaction так, чтобы в консоль вывелось
 // '400 USD - buy on NASDAQ'
 // используйте объект anotherTransaction как контекст
-// ... your code here
-transaction.printTransaction.apply([anotherTransaction]);
+// ... your code heretransaction.printTransaction.apply(anotherTransaction);
