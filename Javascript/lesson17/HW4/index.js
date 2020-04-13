cac18eedc83348fad62d14f60c4ea2f1d6d9e11b
Maryna <maryna.pylypchenko@gmail.com>
2020-04-13 22:03:00 +0300

    commit

---
+++ Javascript/lesson17/HW4/index.js
@@ -0,0 +1,13 @@
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    getFullName() {
+        return (`${this.firstName} ${this.lastName}`);
+    },
+    setFullName(getFullName) {
+        let arr = getFullName.split('');
+        this.firstName = arr[0];
+        this.lastName = arr[1];
+    }
+}
+export { user };
