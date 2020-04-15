98a6cad3cfb9ab8f088f3681da27061b4a50c11a
Maryna <maryna.pylypchenko@gmail.com>
2020-04-15 19:01:09 +0300

    commit

--- Javascript/lesson18/task3/index.js
+++ Javascript/lesson18/task3/index.js
@@ -1,6 +1,6 @@
 const sumOfSquares = () => {
     return [].reduce.apply(arguments, (acc, el) => {
-        return acc + el(el * el);
+        return acc + (el * el);
     }, 0);
 }
 export { sumOfSquares }
