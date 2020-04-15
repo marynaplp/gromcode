6d3619d810a4b8562315bf22f1058c913d2d5a11
Maryna <maryna.pylypchenko@gmail.com>
2020-04-15 21:50:39 +0300

    commit

--- Javascript/lesson18/HW1/index.js
+++ Javascript/lesson18/HW1/index.js
@@ -2,7 +2,7 @@ function saveCalls(allCalls) {
     function withMemory(args) {
 
         withMemory.calls.push([...args]);
-        return allCalls.calls(this, args);
+        return allCalls.calls.apply(this, args);
     }
     withMemory.calls = [];
     return withMemory;
