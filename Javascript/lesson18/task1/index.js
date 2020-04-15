a692472f1fb2ac16055ba3c8e9ff37a9b9e46551
Maryna <maryna.pylypchenko@gmail.com>
2020-04-15 16:48:32 +0300

    commit

--- Javascript/lesson18/task1/index.js
+++ Javascript/lesson18/task1/index.js
@@ -1,21 +1,20 @@
 'use strict';
 const event = {
     guests: [{
-        name: 'John',
+        name: "John",
         age: 18,
-        email: 'example@server.com'
+        email: "example@server.com"
     }],
-    message: 'Welcome to the party!',
+    message: "Welcome to the party!",
     getInvitations() {
         return this.guests
             .filter(({ age }) => age >= 18)
             .map(({ name, email }) => ({
-
-                text: `Dear ${name}! ${this.message}`,
-                email
+                email: `${email}`,
+                message: `Dear ${name}! ${this.message}`,
             }));
+    },
+};
 
-    }
-}
 
 export { event };
