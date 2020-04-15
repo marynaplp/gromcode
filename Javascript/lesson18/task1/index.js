ecdc4ae6d108aabfbe4d0071b9947632b6dfe57b
Maryna <maryna.pylypchenko@gmail.com>
2020-04-15 16:36:37 +0300

    commit

--- Javascript/lesson18/task1/index.js
+++ Javascript/lesson18/task1/index.js
@@ -5,13 +5,12 @@ const event = {
         age: 18,
         email: 'example@server.com'
     }],
-    message: "Welcome to the party",
+    message: 'Welcome to the party!',
     getInvitations() {
         return this.guests
             .filter(({ age }) => age >= 18)
             .map(({ name, email }) => ({
-                text: `Dear ${name}!.${this.message}!`,
-                email
+                text: `Dear ${name}!${this.message}`,
             }));
 
     }
