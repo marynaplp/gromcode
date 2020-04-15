ae3f668263761b913bf129cafc8210e278be8305
Maryna <maryna.pylypchenko@gmail.com>
2020-04-15 18:32:29 +0300

    commit

--- Javascript/lesson18/task2/index.js
+++ Javascript/lesson18/task2/index.js
@@ -3,11 +3,11 @@
 const wallet = {
     transactions: [1, 4, 6, 7],
     getMax() {
-        return Math.max.apply(null, this.transitions);
+        return Math.max.apply(null, this.transactions);
 
     },
     getMin() {
-        return Math.min.apply(null, this.transitions);
+        return Math.min.apply(null, this.transactions);
     }
 
 };
