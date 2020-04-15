4094c5002288f22a9a2295f282af5cac1d2bcdc1
Maryna <maryna.pylypchenko@gmail.com>
2020-04-15 18:29:08 +0300

    commit

--- Javascript/lesson18/task2/index.js
+++ Javascript/lesson18/task2/index.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const wallet = {
-        transactions: [1, 4, 6, 7],
-        getMax() {
-            return Math.max.apply(null, this.transitions);
+    transactions: [1, 4, 6, 7],
+    getMax() {
+        return Math.max.apply(null, this.transitions);
 
-        },
-        getMin() {
-            return Math.min.apply(null, this.transitions);
-        }
-
+    },
+    getMin() {
+        return Math.min.apply(null, this.transitions);
     }
-    //export { wallet };
+
+}
+export { wallet };
