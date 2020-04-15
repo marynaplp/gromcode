a197308ca96a3ad3883b995a037cbf86a90912f8
Maryna <maryna.pylypchenko@gmail.com>
2020-04-15 09:26:21 +0300

    commit

--- Javascript/lesson17/HW5/index.js
+++ Javascript/lesson17/HW5/index.js
@@ -1,30 +1,29 @@
-export const timer = {
-    secondPassed: 0,
+const timer = {
+    secondsPassed: 0,
     minsPassed: 0,
     start: 0,
-
     startTimer() {
         this.start = setInterval(() => {
-            this.secondPassed += 5;
-            if (this.secondPassed == 60) {
-                this.minsPassed += 1,
-                    this.secondPassed = 0;
+            this.secondsPassed += 5;
+            if (this.secondsPassed === 60) {
+                this.minsPassed += 1;
+                this.secondsPassed = 0;
             }
-
-        });
+        }, 5000);
     },
+
     getTime() {
-        if (this.secondsPassed > 9) return `${this.minsPassed}:${this.secondsPassed}`
-        else return `${this.minsPassed}:0${this.secondsPassed}`
+        if (this.secondsPassed > 9)
+            return `${this.minsPassed}:${this.secondsPassed}`;
+        else return `${this.minsPassed}:0${this.secondsPassed}`;
     },
-
     stopTimer() {
-        clearInterval(this.start)
+        clearInterval(this.start);
     },
     resetTimer() {
         this.secondsPassed = 0;
         this.minsPassed = 0;
+    },
+};
 
-    }
-}
-export { timer }
+export { timer };
