3f6f31c2362f7d3c9146e1b887b3146a16314d21
Maryna <maryna.pylypchenko@gmail.com>
2020-04-16 17:48:52 +0300

    commit

--- Javascript/lesson19/task4/index.js
+++ Javascript/lesson19/task4/index.js
@@ -3,17 +3,18 @@ function User(name, age) {
     this.age = age;
 }
 User.prototype.sayHi = function() {
-        console.log(`Hi, I am ${name}`);
+        console.log(`Hi, I am ${this.name}`);
 
     },
     User.prototype.NewPhoto = function() {
-        console.log(`New photo request was sent for ${name}`);
+        console.log(`New photo request was sent for ${this.name}`);
     }
 User.prototype.setAge = function() {
     if (age < 0) return false;
     this.age = age;
     if (age >= 25) {
-        console.log(`New photo request was sent for ${name}`);
+        console.log(`New photo request was sent for ${this.name}`);
     }
     return age
 }
+export { User }
