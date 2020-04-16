0f77fbb4f4498393d51d4539dd2c319205c167c6
Maryna <maryna.pylypchenko@gmail.com>
2020-04-16 16:36:33 +0300

    commit

--- Javascript/lesson19/task3/index.js
+++ Javascript/lesson19/task3/index.js
@@ -2,7 +2,7 @@ const user = {
     firstName: 'John',
     lastName: 'Doe',
     get fullName() {
-        return `${this.firstName}${this.lastName}`;
+        return `${this.firstName} ${this.lastName}`;
     },
     set fullName(value) {
         const [firstName, lastName] = value.split(' '); // даст массив 
