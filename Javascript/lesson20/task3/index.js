2e72a197902806551357e33be35299cf2b307e0f
Maryna <maryna.pylypchenko@gmail.com>
2020-04-17 23:02:15 +0300

    commit

---
+++ Javascript/lesson20/task3/index.js
@@ -0,0 +1,19 @@
+class Wallet {
+    constructor() {
+        this.balance = 0;
+    }
+    getBalance() {
+        return this.balance;
+    }
+    deposit(amount) {
+        return this.balance += amount;
+    }
+    withdraw(amount) {
+        if (amount > this.balance) {
+            console.log('Not enough funds');
+            return;
+        }
+        this.balance -= amount;
+    }
+}
+export { Wallet }
