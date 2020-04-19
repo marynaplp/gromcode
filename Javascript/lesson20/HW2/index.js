2b5a729d43da82ce392b49e03d31462a7d8fcacd
Maryna <maryna.pylypchenko@gmail.com>
2020-04-19 22:05:49 +0300

    commit

--- Javascript/lesson20/HW2/index.js
+++ Javascript/lesson20/HW2/index.js
@@ -7,19 +7,17 @@ class User {
 };
 class UserRepository {
     constructor(users) {
-        return this._users = Object.freeze(users);
+        return (this._users = Object.freeze(users));
     }
-    getUserNames(arr) {
-        return this._name.map(arr(el => el._name));
+    getUserNames() {
+        return this._name.map(arr((el) => el._name));
     }
-    getUserIds(arr) {
-        return _this.id.map(arr(el => el._id));
-
+    getUserIds() {
+        return _this.id.map(arr((el) => el._id));
     }
     getUsedNameById(id) {
-        let findById = this._users.find(i => i._id === _userId);
+        let findById = this._users.find((i) => i._id === _userId);
         return findById;
-
     }
 }
 export { User, UserRepository };
