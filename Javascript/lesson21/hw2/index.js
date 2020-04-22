6aaa2e12fb978133cc12ec1617573cb41199fab8
Maryna <maryna.pylypchenko@gmail.com>
2020-04-22 13:32:00 +0300

    commit

---
+++ Javascript/lesson21/hw2/index.js
@@ -0,0 +1,13 @@
+function getItemsList() {
+    const itemsList = document.querySelectorAll('.technoly')
+    console.dir(elementsList)
+    return itemsList;
+}
+
+function getItemsArray() {
+    const elementsArray = Array.from(document.querySeletorAll('.tool'))
+    console.dir(elementsArray)
+    return elementsArray;
+}
+
+export { getItemsList, getItemsArray }
