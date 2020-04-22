ecbf29437f87360bdd1a12fefafaff91f44260a1
Maryna <maryna.pylypchenko@gmail.com>
2020-04-22 16:13:22 +0300

    commit

---
+++ Javascript/lesson21/hw7/index.js
@@ -0,0 +1,8 @@
+function createButton(buttonText) {
+    const newButton = document.createElement('button');
+    newButton.textContent = '${text}';
+    const buttonElem = document.querySelector('button');
+    buttonElem.append(newButtom);
+
+}
+export { createButton };
