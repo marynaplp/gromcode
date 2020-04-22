2fa2f55f20c6b6628308fa3bfee2b11d8eb0e5c1
Maryna <maryna.pylypchenko@gmail.com>
2020-04-22 16:47:41 +0300

    commit

---
+++ Javascript/lesson21/hw8/index.js
@@ -0,0 +1,25 @@
+function finishList() {
+    const newItem = document.createElement('li');
+    newItem.textContent = '1';
+    const listElem = document.querySelector('.list');
+    listElem.prepend(newItem);
+
+
+    const newItem = document.createElement('li');
+    newItem.textContent = '6';
+    const listElem = document.querySelector('special');
+    listElem.before(newItem);
+
+    const newItem = document.createElement('li');
+    newItem.textContent = '4';
+    const listElem = document.querySelector('special');
+    listElem.after(newItem);
+
+    const newItem = document.createElement('li');
+    newItem.textContent = '8';
+    const listElem = document.querySelector('.list');
+    listElem.append(newItem);
+
+
+}
+export { finishList }
