4e861d07d57e6cbda9c599865ef7850a95365b04
Maryna <maryna.pylypchenko@gmail.com>
2020-04-22 16:53:48 +0300

    commit

--- Javascript/lesson21/hw8/index.js
+++ Javascript/lesson21/hw8/index.js
@@ -1,23 +1,25 @@
 function finishList() {
     const newItem = document.createElement('li');
     newItem.textContent = '1';
-    const listElem = document.querySelector('.list');
+    const listElem = document.querySelector('ul');
     listElem.prepend(newItem);
+
+    const newItem = document.createElement('li');
+    newItem.textContent = '4';
+    const listElem = document.querySelector('special');
+    listElem.before(newItem);
 
 
     const newItem = document.createElement('li');
     newItem.textContent = '6';
     const listElem = document.querySelector('special');
-    listElem.before(newItem);
-
-    const newItem = document.createElement('li');
-    newItem.textContent = '4';
-    const listElem = document.querySelector('special');
     listElem.after(newItem);
+
+
 
     const newItem = document.createElement('li');
     newItem.textContent = '8';
-    const listElem = document.querySelector('.list');
+    const listElem = document.querySelector('ul');
     listElem.append(newItem);
 
 
