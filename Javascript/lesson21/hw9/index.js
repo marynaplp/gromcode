0722370430ae25db0c4922f8af96257bebd2391e
Maryna <maryna.pylypchenko@gmail.com>
2020-04-23 12:08:13 +0300

    commit

--- Javascript/lesson21/hw9/index.js
+++ Javascript/lesson21/hw9/index.js
@@ -5,4 +5,6 @@ function finishForm() {
     const setInput = document.querySelector('.login-form');
     setInput.append(input);
     input.setAttribute('type', 'password');
+    input.setAttribute('name', 'password')
 }
+export { finishForm }
