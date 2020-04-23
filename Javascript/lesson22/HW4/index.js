01aabefc8d7c172483405a74cf55b71300090459
Maryna <maryna.pylypchenko@gmail.com>
2020-04-23 23:38:32 +0300

    commit

---
+++ Javascript/lesson22/HW4/index.js
@@ -0,0 +1,6 @@
+const searchBtn = document.querySelector('.search__btn');
+const searchInput = document.querySelector('.search__input');
+
+addEventListener.searchBtn('click', () => {
+    console.log(earchInput.value)
+});
