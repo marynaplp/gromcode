10a8a700ba56a2b0f987f7669fc2c2053f5504df
Maryna <maryna.pylypchenko@gmail.com>
2020-04-23 22:57:16 +0300

    commit

--- Javascript/lesson22/HW1/index.js
+++ Javascript/lesson22/HW1/index.js
@@ -1,6 +1,6 @@
 const createBtn = document.querySelector('.create-btn');
 
-function aaddEventListener() {
+function addEventListener() {
     alert(done);
 }
-createBtn.addEventListener('click')
+createBtn.addEventListener('click', alert(done))
