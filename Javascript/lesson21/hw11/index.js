67b1ce24c738c603c0a1fecf46565ebcfbcef855
Maryna <maryna.pylypchenko@gmail.com>
2020-04-23 14:17:48 +0300

    commit

--- Javascript/lesson21/hw11/index.js
+++ Javascript/lesson21/hw11/index.js
@@ -1,7 +1,7 @@
 function squaredNumbers() {
     const squaredNum = document.querySelectorAll('.number');
     squaredNum.forEach(function(el) {
-        el.dataset.squaredNum = el.dataset * el.dataset
+        el.dataset.squaredNum = el.dataset.number * el.dataset.number
     });
 
 }
