86d7a5bc163246f93b41d222c3aaf53909d08f12
Maryna <maryna.pylypchenko@gmail.com>
2020-04-24 13:55:43 +0300

    commit

--- Javascript/lesson22/HW7/index.js
+++ Javascript/lesson22/HW7/index.js
@@ -1,6 +1,6 @@
 const inPutValue = document.querySelector('.text-input');
 
 const inPut = (event) => {
-    console.log(event.target.value)
+    console.log(event.target.checked)
 };
 inPutValue.addEventListener('change', inPut)
