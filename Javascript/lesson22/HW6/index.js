b0236c6e3dac0b82a34de6e55a15ace86aef29cf
Maryna <maryna.pylypchenko@gmail.com>
2020-04-24 11:59:09 +0300

    commit

--- Javascript/lesson22/HW6/index.js
+++ Javascript/lesson22/HW6/index.js
@@ -1,11 +1,9 @@
 const buttons = document.querySelectorAll('.btn');
 
-const handleClick = (event) => {
+const handleClick = (event) =>
     console.log(event.target.textContent);
 
-    buttons.forEach(button => {
-        button.addEventListener('click', handleClick)
+buttons.forEach(button => {
+    button.addEventListener('click', handleClick)
 
-    });
-
-};
+});
