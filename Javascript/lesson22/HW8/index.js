114fac2b94070a1d1b87e6a33214054c3c157cc9
Maryna <maryna.pylypchenko@gmail.com>
2020-04-24 14:35:39 +0300

    commit

--- Javascript/lesson22/HW8/index.js
+++ Javascript/lesson22/HW8/index.js
@@ -1,9 +1,8 @@
 const checkBox = document.querySelector('.task-status');
-
 const checkbox = (event) => {
     console.log(event.target.checked)
 };
 
-checkBox.addEventListener('change', () => {
-    checkbox.checked ? console.log(true) : console.log(false);
+checkbox.addEventListener('change', () => {
+    checkBox.checked ? console.log(true) : console.log(false);
 });
