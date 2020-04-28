14175ab9be78a0caf91187b7553275fa2d82ea5f
Maryna <maryna.pylypchenko@gmail.com>
2020-04-28 16:30:55 +0300

    commit

---
+++ Javascript/lesson24/task2/index.js
@@ -0,0 +1,11 @@
+const formatter = new Intl.DateTimeFormat('en', {
+    timeZone: 'UTC',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+
+});
+const getGreenwichTime = date => formatter.format(date);
+console.log(getGreenwichTime(new Date()));
+
+export { getGreenwichTime }
