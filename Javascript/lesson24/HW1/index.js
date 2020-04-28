7d45139b803a66aa8e154009f2992200883f1bf3
Maryna <maryna.pylypchenko@gmail.com>
2020-04-28 19:31:48 +0300

    commit

--- Javascript/lesson24/HW1/index.js
+++ Javascript/lesson24/HW1/index.js
@@ -2,7 +2,7 @@ const getDiff = (startDate, endDate) => {
     let result = new Date(startDate) - new Date(endDate);
 
     if (startDate < endDate)
-        result = new Date(startDate) - new Date(endDate);
+        result = new Date(endDate) - new Date(startDate);
     const diffDay = parseInt(result / (1000 * 60 * 60 * 24));
     const diffHour = parseInt((result / (1000 * 60 * 60)) % 24);
     const diffMin = parseInt((result / (1000 * 60)) % 60);
