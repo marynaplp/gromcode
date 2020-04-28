e74d8e585b2135d44c0e9c7f8a797a1534738fd7
Maryna <maryna.pylypchenko@gmail.com>
2020-04-28 15:47:36 +0300

    commit

--- Javascript/lesson24/task1/index.js
+++ Javascript/lesson24/task1/index.js
@@ -7,5 +7,8 @@ const dayOfWeek = (date, days) => {
     return weekDays[new Date(dateInFuture).getDay()];
 };
 
+const result = dayOfWeek(new Date(2020, 0, 1), n);
+
+console.log(result);
 
 export { dayOfWeek }
