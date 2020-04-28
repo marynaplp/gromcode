cbcbfacf78e052a42727a5eea05c43d5ad2e6c0e
Maryna <maryna.pylypchenko@gmail.com>
2020-04-28 15:49:47 +0300

    commit

--- Javascript/lesson24/task1/index.js
+++ Javascript/lesson24/task1/index.js
@@ -1,5 +1,5 @@
 const dayOfWeek = (date, days) => {
-    const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+    const weekDays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
 
     const day = new Date(date).getDate();
     const dateInFuture = new Date(date).setDate(day + days);
