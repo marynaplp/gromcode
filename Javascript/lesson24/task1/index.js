de20238e100d600e84cd26e801673992c6e10b23
Maryna <maryna.pylypchenko@gmail.com>
2020-04-28 15:30:34 +0300

    commit

--- Javascript/lesson24/task1/index.js
+++ Javascript/lesson24/task1/index.js
@@ -1,11 +1,12 @@
-const weekDays = ['Su', 'Mon', 'Tu', 'Wed', 'Thu', 'Fri', 'St'];
 const dayOfWeek = (date, days) => {
+    const weekDays = ['Su', 'Mon', 'Tu', 'Wed', 'Thu', 'Fri', 'St'];
+
     const day = new Date(date).getDate();
     const dateInFuture = new Date(date).setDate(day + days);
 
     return weekDays[new Date(dateInFuture).getDay()];
 }
-const result = dayOfWeek(new Date(2020, 0, 1), 0);
+const result = dayOfWeek(new Date(2020, 0, 1), 14);
 
 console.log(result);
 
