5049140b792a5e7cc17fee0cdbb776a8877f0d6f
Maryna <maryna.pylypchenko@gmail.com>
2020-04-29 13:55:46 +0300

    commit

--- Javascript/lesson24/HW2/index.js
+++ Javascript/lesson24/HW2/index.js
@@ -1,16 +1,25 @@
-const students = [
-    { name: 'Tom', birthday: '01/15/2010' },
-    { name: 'Ben', birthday: '01/17/2008' },
-    { name: 'Sam', birthday: '03/15/2010' },
-
-]
+//const students = [
+//  { name: 'Tom', birthday: '01/15/2010' },
+//  { name: 'Ben', birthday: '01/17/2008' },
+// { name: 'Sam', birthday: '03/15/2010' },
+//
+//]
 
 const studentsBirthDays = students => {
 
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    // const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
     const sortedStudents = students.sort((a, b) => new Date(a).getDate() - new Date(b).getDate())
-    const newArray = sortedStudents.reduce
+    const newArray = sortedStudents.reduce((acc, {
+        name,
+        birthday
+    }) => {
+        const nameMonth = months[new Date(birthday).getMonth()];
+        return { acc, [nameMonth]: acc[nameMonth] ? acc[nameMonth].concat(name) : [name] }
+
+    }, {});
+
+
     return newArray
 }
-console.log(studentsBirthDays(students));
+console.log(studentsBirthDays(students))
