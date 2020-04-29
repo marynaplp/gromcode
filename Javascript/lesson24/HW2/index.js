639d21f4bb89c7a81cde81682b2e36c05045b2c6
Maryna <maryna.pylypchenko@gmail.com>
2020-04-29 15:00:16 +0300

    commit

--- Javascript/lesson24/HW2/index.js
+++ Javascript/lesson24/HW2/index.js
@@ -1,19 +1,19 @@
 const students = [
-    { name: 'Tom', birthDay: '01/15/2010' },
-    { name: 'Ben', birthDay: '01/17/2008' },
-    { name: 'Sam', birthDay: '03/15/2010' },
+    { name: 'Tom', birthDate: '01/15/2010' },
+    { name: 'Ben', birthDate: '01/17/2008' },
+    { name: 'Sam', birthDate: '03/15/2010' },
 ]
 
 const studentsBirthDays = students => {
 
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    const sortedStudents = students.sort((a, b) => new Date(a.birthDay).getDate() - new Date(b.birthDay).getDate())
+    const sortedStudents = students.sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
     const newArray = sortedStudents.reduce((acc, {
         name,
-        birthDay
+        birthDate
     }) => {
-        const nameMonth = months[new Date(birthDay).getMonth()];
+        const nameMonth = months[new Date(birthDate).getMonth()];
         return {...acc, [nameMonth]: acc[nameMonth] ? acc[nameMonth].concat(name) : [name] }
 
     }, {});
