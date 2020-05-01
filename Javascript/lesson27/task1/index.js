a75116017d7ce7d805846b0e98919ca4ca19c8cc
Maryna <maryna.pylypchenko@gmail.com>
2020-05-01 20:12:21 +0300

    commit

--- Javascript/lesson27/task1/index.js
+++ Javascript/lesson27/task1/index.js
@@ -6,9 +6,15 @@
  const getLocalStorageData = () => {
      return Object.entries(localStorage)
          .reduce((acc, [key, value]) => {
+             let newValue;
+             try {
+                 newValue = JSON.parse(value);
+             } catch (e) {
+                 newValue = value;
+             }
              return {
                  ...acc,
-                 [key]: JSON.parse(value),
+                 [key]: newValue,
              };
          }, {});
 
