d63125af969db7935689fec83cea3c6bec114da1
Maryna <maryna.pylypchenko@gmail.com>
2020-05-04 16:06:36 +0300

    commit

--- Javascript/lesson28/task1/index.js
+++ Javascript/lesson28/task1/index.js
@@ -25,8 +25,6 @@ const calc = initValue => {
             return result
         }
 
-    }
+    };
     return calculator
 };
-const result = calc(3).add(2).mult(4).div(10).substract(5).result();
-console.log(result)
