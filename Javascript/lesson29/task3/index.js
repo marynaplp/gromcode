66752e79d836635fb9f9bbb3b2f42859bbdc65c6
Maryna <maryna.pylypchenko@gmail.com>
2020-05-07 15:50:31 +0300

    commit

--- Javascript/lesson29/task3/index.js
+++ Javascript/lesson29/task3/index.js
@@ -1,7 +1,8 @@
-export const printing = () => {
-    console.log(1);
-    setTimeout(function() { console.log(2); }, 1000);
-    console.log(5);
-    setTimeout(function() { console.log(4); }, 0);
-    console.log(3);
-}
+ const printing = () => {
+     console.log('1');
+     setTimeout(() => console.log('5'), 1000);
+     console.log('2');
+     setTimeout(() => console.log('4'), 0);
+     console.log('3');
+ }
+ printing()
