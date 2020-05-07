791def017a5e3259938497a670a80b06386385df
Maryna <maryna.pylypchenko@gmail.com>
2020-05-07 15:22:30 +0300

    commit

---
+++ Javascript/lesson29/task3/index.js
@@ -0,0 +1,7 @@
+export const priting = () => {
+    console.log(1);
+    setTimeout(function() { console.log(2); }, 1000);
+    console.log(5);
+    setTimeout(function() { console.log(4); }, 0);
+    console.log(3);
+}
