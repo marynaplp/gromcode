0a6844c70e252f5af5de97d32e2cdb82f21aea0d
Maryna <maryna.pylypchenko@gmail.com>
2020-05-08 16:27:54 +0300

    commit

---
+++ Javascript/lesson29/HW1/  index.js
@@ -0,0 +1,13 @@
+const requestUserData = (userId, callback) => {
+    if (userId == 'broken') {
+        callback(null, 'Failed to load user data');
+        return;
+    }
+    let object = {
+        name: 'John',
+        age: 17,
+        userId,
+        email: 'userid777@example.com'
+    };
+    callback = (object);
+}
