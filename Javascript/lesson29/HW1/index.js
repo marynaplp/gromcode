69963d24cdb8bce2f10b6e899b15cc136e5097ac
Maryna <maryna.pylypchenko@gmail.com>
2020-05-09 14:23:28 +0300

    commit

--- Javascript/lesson29/HW1/index.js
+++ Javascript/lesson29/HW1/index.js
@@ -1,4 +1,4 @@
-const getRandomDelay = (form, to) => from + Math.random() * (from - to);
+const getRandomDelay = (from, to) => from + Math.random() * (from - to);
 const requestUserData = (userId, callback) => {
     const randomDelay = getRandomDelay(1000, 3000);
     if (userId === "broken") {
