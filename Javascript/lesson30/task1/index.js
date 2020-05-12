264ad3ba667510fd505df08021fa25c4e2e76358
Maryna <maryna.pylypchenko@gmail.com>
2020-05-12 15:39:00 +0300

    commit

---
+++ Javascript/lesson30/task1/index.js
@@ -0,0 +1,41 @@
+caddoImageWithPromise = imgSrc => {
+    const p = new Promise((resolveCb, rejectCb) => {
+        const imgElem = document.createElement('img');
+        imgElem.setAttribute('alt', 'My Photo');
+        imgElem.src = imgSrc;
+        const containerElem = document.querySelector('.page');
+        containerElem.append(imgElem);
+
+        function onImageLoaded() {
+            const {
+                width,
+                height
+            } = imgElem;
+            resolveCb({
+                width,
+                height
+            });
+        };
+
+        imgElem.addEventListener('load', onImageLoaded);
+        imgElem.addEventListener('error', () => rejectCb(new Error('Image load failed')));
+    })
+    return p;
+};
+
+//function onImageLoaded(error, data) {
+// if (error) {
+//     console.log(error);
+//     return;
+// }
+// const {
+//  width,
+// height
+//} = data;
+//const sizeElem = document.querySelector('.image-size');
+//  sizeElem.textContent = `${width} x ${height}`;
+//};
+
+//export {
+//  addImage
+//};
