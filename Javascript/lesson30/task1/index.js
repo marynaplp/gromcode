beced6651e5020037c5185416a101482e5b119a6
Maryna <maryna.pylypchenko@gmail.com>
2020-05-12 15:48:41 +0300

    commit

--- Javascript/lesson30/task1/index.js
+++ Javascript/lesson30/task1/index.js
@@ -1,4 +1,4 @@
-const addImagee = imgSrc => {
+const addImage = imgSrc => {
     const p = new Promise((resolveCb, rejectCb) => {
         const imgElem = document.createElement('img');
         imgElem.setAttribute('alt', 'My Photo');
