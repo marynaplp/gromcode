4040a9238dfc77c8571c3a6211483187525f45bf
Maryna <maryna.pylypchenko@gmail.com>
2020-05-12 21:34:50 +0300

    commit

--- Javascript/lesson30/HW1/index.js
+++ Javascript/lesson30/HW1/index.js
@@ -23,17 +23,18 @@ console.log(userDataPromise);
 userDataPromise
     .then(function onSuccess(userData) {
         /* ...code here */
+        console.log(userData)
     });
-console.log(userData)
-    /*
-     * подпишитесь на успешное выполнение промиса userDataPromise
-     * используйте метод .then
-     * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
-     * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
-     */
+
+/*
+ * подпишитесь на успешное выполнение промиса userDataPromise
+ * используйте метод .then
+ * в обработчике промиса (ф-ция внутри .then() ) выведите в консоль строку 'My name is Tom. I am 17 years old'
+ * Tom и 17 достаньте с данных, которые приходят в ф-цию onSuccess
+ */
 
 userDataPromise
     .then(function onSuccess( /* ...code here */ ) {
         /* ...code here */
-        console.log(`My name is ${userData.name}. I am ${userData.age}years old`)
+        console.log(`My name is ${userData.name}. I am ${userData.age} years old`);
     });
