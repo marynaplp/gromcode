5f593813d25dd8a241664766e9dac3942e6351ef
Maryna <maryna.pylypchenko@gmail.com>
2020-05-14 17:01:55 +0300

    commit

---
+++ Javascript/lesson31/task2/index.js
@@ -0,0 +1,21 @@
+export const asyncCalculator = num => newPromise((resolve) => {
+        setTimeout(() => {
+            console.log(`Initial value : ${num}`);
+            resolve(num);
+
+        }, 500);
+    })
+    .then(value => new Promise((resolve) => {
+        setTimeout(() => {
+            const result = value * value;
+            console.log(`Squared value:${result}`);
+            resolve(result);
+        }, 500);
+    }))
+    .then(value => new Promise((resolve) => {
+        setTimeout(() => {
+            const result = value * value;
+            console.log(`Doubled value: ${result}`);
+            resolve(result);
+        }, 500);
+    }))
