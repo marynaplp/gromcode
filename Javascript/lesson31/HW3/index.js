f44ec124a3df4805aec792ac163fcfdaa5a507f0
Maryna <maryna.pylypchenko@gmail.com>
2020-05-14 21:02:54 +0300

    commit

--- Javascript/lesson31/HW3/index.js
+++ Javascript/lesson31/HW3/index.js
@@ -1,10 +1,9 @@
 export const delay = time => {
-    new Promise((resolve) => {
-        setTimeout(() => {
-            console.log(delay);
-            resolve(delay);
-        }, time)
-    });
-}
-delay(3000)
-    .then(() => console.log("Done"));
+        new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(delay);
+                }, time)
+            }
+        };
+        delay(3000)
+            .then(() => (console.log('Done')))
