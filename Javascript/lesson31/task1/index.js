8db01ca2802cd56f8e7b3b31fae71b1bc628e02a
Maryna <maryna.pylypchenko@gmail.com>
2020-05-14 15:35:39 +0300

    commit

---
+++ Javascript/lesson31/task1/index.js
@@ -0,0 +1,23 @@
+const requestUserData = userId => {
+    const request = new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(new Error("User not found"));
+            }, 500);
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: "John",
+                    age: 17,
+                    email: `${userId}@example.com`,
+                    userId: `${userId}`
+                });
+            }, 1000);
+        }
+    });
+    return request;
+}
+requestUserData('broken')
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+    .finally(() => console.log('finally'));
