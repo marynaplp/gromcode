4174dfe424a05dd5ad4af9f654ff838361cf41c8
Maryna <maryna.pylypchenko@gmail.com>
2020-05-17 18:10:35 +0300

    commit

--- Javascript/lesson32/task2/index.js
+++ Javascript/lesson32/task2/index.js
@@ -1,1 +1,30 @@
-const getUserAsap
+const getRandomNumber = (from, to) =>
+    from + Math.random() * (to - from);
+const request = url => newPromise(resolve => {
+    const randomDelay = getRandomNumber(1000, 3000)
+    setTimeout(() => {
+        resolve({
+            userData: {
+                name: 'Tom',
+                age: 17,
+            },
+            source: url
+        });
+    }, randomDelay);
+});
+
+const servers = [
+    'https://server.com/us',
+    'https://server.com/eu',
+    'https://server.com/au',
+
+];
+const getUserASAP = userId => {
+    const userUrls = servers
+        .map(serverUrl => `${serverUrl}/users/${userId}`);
+    const requests = userUrls
+        .map(userUrl => request(usUrl))
+    return Promise.race(requests);
+};
+getUserASAP('user-id-1')
+    .then(res => console.log(res))
