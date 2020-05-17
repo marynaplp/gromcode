1e61dd39efcbbf8911a9cda4cea28c07ecc856c4
Maryna <maryna.pylypchenko@gmail.com>
2020-05-17 17:45:57 +0300

    commit

--- Javascript/lesson32/HW2/index.js
+++ Javascript/lesson32/HW2/index.js
@@ -7,7 +7,7 @@ const promiseNumber3 = Promise.resolve(8);
  * чтобы в консоль вывелась сумма всех чисел из трех промисов
  */
 export const resultPromise = (promiseNumber1, promiseNumber2, promiseNumber3) =>
-    Promise.all(promiseNumber1, promiseNumber2, promiseNumber3)
+    Promise.all([promiseNumber1, promiseNumber2, promiseNumber3])
     /* ...code here */
 
 resultPromise
