d02e5f98e2129bc2ef606fabe32433e6bccab2a7
Maryna <maryna.pylypchenko@gmail.com>
2020-05-20 15:23:45 +0300

    commit

--- Javascript/lesson33/HW1/index.js
+++ Javascript/lesson33/HW1/index.js
@@ -3,11 +3,11 @@ const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 /* getTasksList code here */
 
 export const getTasksList = () => {
-    return fetch(url)
+    return fetch(baseUrl)
         .then(response => response.json());
 };
 /* getTaskById code here */
 export const getTaskById = taskId => {
-    return fetch(`${url}/${taskId}`)
+    return fetch(`${baseUrl}/${taskId}`)
         .then(response => response.json());
 };
