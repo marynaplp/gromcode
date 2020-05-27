85beb33df3f1edb86a930ac97fa3b7de86b1b2c5
Maryna <maryna.pylypchenko@gmail.com>
2020-05-28 00:24:28 +0300

    commit

--- Javascript/lesson34/HW1/index.js
+++ Javascript/lesson34/HW1/index.js
@@ -5,16 +5,16 @@ export const getUsersList = () => {
         .then(response => response.json());
 };
 
-export const getUsersById = (userId) => {
+export const getUserById = userId => {
     return fetch(`${baseUrl}/${userId}`)
         .then(response => response.json());
 };
 
-export const createUser = (user) => {
+export const createUser = user => {
     return fetch(baseUrl, {
         method: 'POST',
         headers: {
-            'Content -Type': 'application/json;charser= utf-8'
+            'Content-Type': 'application/json;charser= utf-8'
         },
         body: JSON.stringify(user)
 
@@ -25,7 +25,7 @@ export const updateUser = (id, updatedUserData) => {
     return fetch(`${baseUrl}/${id}`, {
         method: 'PUT',
         headers: {
-            'Content -Type': 'application/json;charser= utf-8'
+            'Content-Type': 'application/json;charser= utf-8'
         },
         body: JSON.stringify(updatedUserData)
 
@@ -33,7 +33,7 @@ export const updateUser = (id, updatedUserData) => {
 
 }
 
-export const deleteUser = (id) => {
+export const deleteUser = id => {
     return fetch(`${baseUrl}/${id}`, {
         method: 'DELETE',
     });
