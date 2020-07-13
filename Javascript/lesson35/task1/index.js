d3bb9438557ee31bb7826decfc1eb7bea988d371
Maryna <maryna.pylypchenko@gmail.com>
2020-07-13 15:56:10 +0300

    commit

---
+++ Javascript/lesson35/task1/index.js
@@ -0,0 +1,24 @@
+window.addEventListener('error', function onUnhandledError(err) {
+    console.log(err)
+});
+let userParsingResult;
+try {
+    const user = JSON.parse('{"name":"Maryna"}')
+    console.log("User data: ", user);
+    userParsingResult = "success";
+} catch (e) {
+    userParsingResult = "error";
+} finally {
+    console.log(`User parsing finished with ${userParsingResult}`);
+}
+
+let productParsingResult;
+try {
+    const product = JSON.parse('{"name":"Maryna"}');
+    console.log("Product data: ", product);
+    productParsingResult = "success";
+} catch (e) {
+    productParsingResult = "error";
+} finally {
+    console.log(`Product parsing finished with ${productParsingResult}`);
+}
