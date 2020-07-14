3eaa7c1385af691727e9a18a96c13b19747674f9
Maryna <maryna.pylypchenko@gmail.com>
2020-07-14 14:01:17 +0300

    commit

--- Javascript/lesson35/task3/gateways.js
+++ Javascript/lesson35/task3/gateways.js
@@ -1,19 +1,20 @@
 // запрашиваем пользователя
 export const fetchUserData = userName => {
-    return fetch(`https://api.github.com/users/${userName}`)
-        .then(response => {
-            if (response.ok) {
-                return response.json()
-            }
-            throw new Error('Failed to load data');
-        });
+    const response = await fetch(`https://api.github.com/users/${userName}`)
+        //.then(response => {
+    if (response.ok) {
+        return await response.json()
+    }
+    throw new Error('Failed to load data');
+
 };
 
-export const fetchRepositories = url =>
-    fetch(url)
-    .then(response => {
-        if (response.ok) {
-            return response.json()
-        }
-        throw new Error('Failed to load data');
-    });
+export const fetchRepositories = async url => {
+    const response = await fetch(url)
+
+
+    if (response.ok) {
+        return await response.json();
+    }
+    throw new Error('Failed to load data');
+}
