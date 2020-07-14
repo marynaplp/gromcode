3ae2f8f96639ad49559ea35014ff63933d18a486
Maryna <maryna.pylypchenko@gmail.com>
2020-07-14 16:10:07 +0300

    commit

--- Javascript/lesson36/HW1/index.js
+++ Javascript/lesson36/HW1/index.js
@@ -1,15 +1,15 @@
 export const getUsersBlogs = async users => {
     try {
         const getUsersData = await users.forEach(users => {
-            fetch(`https://api.github.com/users/${userId}`)
-                .then(response => {
-                    if (response.ok) {
-                        return response.json();
-                    }
-                })
-            throw new Error('Failed to fetch user')
-        })
-
+                fetch(`https://api.github.com/users/${userId}`)
+                    .then(response => {
+                        if (response.ok) {
+                            return response.json();
+                        }
+                    })
+                throw new Error('Failed to fetch user')
+            })
+            .then(users => users.blog)
         const userData = Promise.all(getUsersData);
         return userData;
     } catch (error) {
