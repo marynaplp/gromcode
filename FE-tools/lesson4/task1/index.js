4422a89dead764e13fd26724920e065308a717bf
Maryna <maryna.pylypchenko@gmail.com>
2020-07-21 19:08:59 +0300

    commit

--- FE-tools/lesson4/task1/index.js
+++ FE-tools/lesson4/task1/index.js
@@ -9,18 +9,6 @@ import {
 import './polyfills/array-flat.js';
 import './polyfills/array-flatMap.js';
 
-
-import calcExpression, {
-    sum,
-    mult
-} from "./calculator/index.js";
-import fetchUser from "./profile/gateway.js";
-import {
-    printProfile
-} from "./profile/index.js";
-import './polyfills/array-flat.js';
-import './polyfills/array-flatMap.js';
-
 const calcResult = calcExpression('1 + 2');
 const sumResult = sum(1, 2);
 const multResult = mult(1, 2);
