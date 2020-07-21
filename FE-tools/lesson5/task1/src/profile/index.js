811499b383c0dc438eaf86a1bcd2eee549d87c2f
Maryna <maryna.pylypchenko@gmail.com>
2020-07-21 23:55:39 +0300

    co

---
+++ FE-tools/lesson5/task1/src/profile/index.js
@@ -0,0 +1,9 @@
+// file path: src/profile/index.js
+
+export const printProfile = profileData => {
+    const {
+        name,
+        company
+    } = profileData;
+    console.log(`${name} from ${company}`);
+};
